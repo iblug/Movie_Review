9b80ac6ba71c6649299d3b7bf4aed14dbee43a67
chae-yoon <munimooo9765@gmail.com>
2023-04-14 14:58:48 +0900

    Edit profile page

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout, name='logout'),
-    path('profile/<int:user_pk>', views.profile, name='profile'),
+    path('profile/<int:user_pk>/', views.profile, name='profile'),
 ]
