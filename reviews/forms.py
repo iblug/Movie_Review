4946cfbda4524b31d3be9e5840eb627eca246cf3
iblug <1115jkb@gmail.com>
2023-04-14 16:54:45 +0900

    Modify form custom form

--- reviews/forms.py
+++ reviews/forms.py
@@ -2,6 +2,39 @@ from django import forms
 from .models import Review, Comment
 
 class ReviewForm(forms.ModelForm):
+    title = forms.CharField(
+        label='제목',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': '제목을 입력해주세요',
+            }
+        ),
+    )
+    content = forms.CharField(
+        label='내용',
+        widget=forms.Textarea(
+            attrs={
+                'class': 'form-control',
+                'placeholder': '내용을 입력해주세요',
+            }
+        ),
+    )
+    movie = forms.CharField(
+        label='영화 이름',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': '영화이름을 입력해주세요',
+            }
+        ),
+    )
+    image = forms.ImageField(
+        label='사진',
+        widget=forms.FileInput(
+            
+        )
+    )
     class Meta:
         model = Review
         fields = ('title', 'content', 'movie', 'image',)
