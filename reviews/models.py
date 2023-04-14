d59c3a80f2c02b802ca0f2fb9f567e0dbbf3f902
ParkJjHwan22 <bat522@naver.com>
2023-04-14 16:11:41 +0900

    Modify comment

--- reviews/models.py
+++ reviews/models.py
@@ -14,3 +14,4 @@ class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     review = models.ForeignKey(Review, on_delete=models.CASCADE)
     content = models.TextField(null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
