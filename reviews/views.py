920bbef9413b562993fb8a261a59868572a2acc9
iblug <1115jkb@gmail.com>
2023-04-14 13:52:42 +0900

    Add review-img

--- reviews/views.py
+++ reviews/views.py
@@ -15,7 +15,7 @@ def index(request):
 @login_required
 def create(request):
     if request.method == 'POST':
-        form = ReviewForm(request.POST)
+        form = ReviewForm(request.POST, request.FILES)
         if form.is_valid():
             review = form.save(commit=False)
             review.user = request.user
@@ -47,7 +47,7 @@ def update(request, review_pk):
     
     if request.user == review.user:
         if request.method == 'POST':
-            form = ReviewForm(request.POST, instance=review)
+            form = ReviewForm(request.POST, request.FILES, instance=review)
             if form.is_valid():
                 form.save()
                 return redirect('reviews:detail', review.pk)
